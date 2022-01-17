acf6dcc5e4dcc2d8cc58ca9b235760bbed9a03d7
Matt Miller <matt@miller-media.com>
2022-01-17 14:06:06 -0800

    Update directional controls

--- src/controller.cpp
+++ src/controller.cpp
@@ -9,6 +9,10 @@ void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
   return;
 }
 
+/*
+ * This function has been updated to prevent the user
+ * from going backwards into its own body
+ */
 void Controller::HandleInput(bool &running, Snake &snake) const {
   SDL_Event e;
   while (SDL_PollEvent(&e)) {
@@ -17,23 +21,31 @@ void Controller::HandleInput(bool &running, Snake &snake) const {
     } else if (e.type == SDL_KEYDOWN) {
       switch (e.key.keysym.sym) {
         case SDLK_UP:
-          ChangeDirection(snake, Snake::Direction::kUp,
-                          Snake::Direction::kDown);
+            if(snake.size == 1 || (snake.direction != Snake::Direction::kDown || snake.direction != Snake::Direction::kUp)) {
+                ChangeDirection(snake, Snake::Direction::kUp,
+                                Snake::Direction::kDown);
+            }
           break;
 
         case SDLK_DOWN:
-          ChangeDirection(snake, Snake::Direction::kDown,
-                          Snake::Direction::kUp);
+            if(snake.size == 1 || (snake.direction != Snake::Direction::kDown || snake.direction != Snake::Direction::kUp)) {
+                ChangeDirection(snake, Snake::Direction::kDown,
+                                Snake::Direction::kUp);
+            }
           break;
 
         case SDLK_LEFT:
-          ChangeDirection(snake, Snake::Direction::kLeft,
-                          Snake::Direction::kRight);
+            if(snake.size == 1 || (snake.direction != Snake::Direction::kLeft || snake.direction != Snake::Direction::kRight)) {
+                ChangeDirection(snake, Snake::Direction::kLeft,
+                                Snake::Direction::kRight);
+            }
           break;
 
         case SDLK_RIGHT:
-          ChangeDirection(snake, Snake::Direction::kRight,
-                          Snake::Direction::kLeft);
+            if(snake.size == 1 || (snake.direction != Snake::Direction::kLeft || snake.direction != Snake::Direction::kRight)) {
+                ChangeDirection(snake, Snake::Direction::kRight,
+                                Snake::Direction::kLeft);
+            }
           break;
       }
     }
